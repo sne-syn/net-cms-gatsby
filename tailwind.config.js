cddc86e0dafff98abbe64d06d7e73a7be6b3e4a5
sne-syn <snezhana.syngur@gmail.com>
2021-04-11 20:32:17 +0200

    Update fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,7 @@
 const defaultTheme = require("tailwindcss/defaultTheme")
 
 module.exports = {
-  theme: {
-    extend: {
-      fontFamily: {
-        serif: ["Merriweather", ...defaultTheme.fontFamily.serif],
-        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
-      },
-    },
-  },
+  theme: {},
   variants: {},
   plugins: [],
 }
