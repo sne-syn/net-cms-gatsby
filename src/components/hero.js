2e58351f0f6f3b1260d8aa92280184dcf21aeaff
sne-syn <snezhana.syngur@gmail.com>
2021-04-19 22:30:41 +0200

    Changes

--- src/components/hero.js
+++ src/components/hero.js
@@ -26,11 +26,12 @@ const Hero = () => {
     fluid={imageData}
     backgroundColor={`#040e18`}
     >
+      <div className="h-32"></div>
+      <h2 class="w-3/4 mx-auto text-white text-center text-5xl">The really empty space: do you need actors to make theatre?</h2>
+      <p class="w-3/4 mx-auto text-white text-center text-3xl">Theatre is often framed in terms of presence – the liveness of a performance – so its omission can feel glaringly apparent. So much so that the absence itself can have a presence.</p>
       <div className="h-96"></div>
-      <h2 class="w-3/4 sm:w-auto my-auto text-white text-center text-5xl">The really empty space: do you need actors to make theatre?</h2>
-      <p class="w-3/4 sm:w-auto my-auto text-white text-center text-3xl">Theatre is often framed in terms of presence – the liveness of a performance – so its omission can feel glaringly apparent. So much so that the absence itself can have a presence.</p>
-      <div className="h-96"></div>
-      
+      <div className="h-32"></div>
+      <div className="h-32"></div>
     </BackgroundImage>
   )
 }
